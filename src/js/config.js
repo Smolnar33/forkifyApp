fe2b78852c221a81eb88c5a069da49f664d0a8ce
Smolnar33 <mol.sandor.92@gmail.com>
2023-04-09 13:43:49 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3b086e33-ca3a-4545-8c35-20d5d6d006a8';
+export const MODAL_CLOSE_SECONDS = 2.5;
